6bcc1d615106de4ddf179a781bc32b2c38c040e6
mikhail-luginin <mlugnn@gmail.com>
2023-04-23 15:51:58 +0300

    Fix decorator

--- modules/admin/decorators.py
+++ modules/admin/decorators.py
@@ -1,12 +1,12 @@
 from aiogram import types
-from aiogram.dispatcher import FSMContext
 
 from . import validators
 
 
-async def check_admin(handler):
-    async def wrapped(message: types.Message, state: FSMContext):
+def check_admin(handler):
+    async def wrapped(message: types.Message, *args, **kwargs):
         sender_username = message.from_user.username
         validate_status = await validators.validate_is_admin(sender_username)
-        await message.answer(text='You are not admin :(') if not validate_status else await handler(message, state)
+        await message.answer(text='You are not admin :(') if not validate_status else await handler(message,
+                                                                                                    *args, **kwargs)
     return wrapped
